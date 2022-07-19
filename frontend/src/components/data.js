085b4efec48f89be7198d27ee9114bab3a8cae8e
binarflow <clouds.overflow@gmail.com>
2022-07-19 15:28:22 -0400

    first

---
+++ frontend/src/components/data.js
@@ -0,0 +1,42 @@
+const data = {
+  products: [
+    {
+      name: "ECHEVERIA PERLE VON NURNBERG",
+      slug: "ECHEVERIA-PERLE-VON-NURNBERG",
+      category: "succulent",
+      image: "/images/1.webp",
+      price: 5.75,
+      inStockCount: 10,
+      rating: 4.5,
+      reviewsCount: 10,
+      description:
+        "This succulent is a beautifully colored Echeveria Perle Von Nurnberg. The plant is a hybrid of E. Gibbiflora v. Metallica and E. Potosina.",
+    },
+    {
+      name: "ECHEVERIA PERLE VON NURNBERG",
+      slug: "ECHEVERIA-PERLE-VON-NURNBERG",
+      category: "succulent",
+      image: "/images/1/.webp",
+      price: 5.75,
+      inStockCount: 10,
+      rating: 4.5,
+      reviewsCount: 10,
+      description:
+        "This succulent is a beautifully colored Echeveria Perle Von Nurnberg. The plant is a hybrid of E. Gibbiflora v. Metallica and E. Potosina.",
+    },
+    {
+      name: "ECHEVERIA PERLE VON NURNBERG",
+      slug: "ECHEVERIA-PERLE-VON-NURNBERG",
+      category: "succulent",
+      image: "/images/1.webp",
+      price: 5.75,
+      inStockCount: 10,
+      rating: 4.5,
+      reviewsCount: 10,
+      description:
+        "This succulent is a beautifully colored Echeveria Perle Von Nurnberg. The plant is a hybrid of E. Gibbiflora v. Metallica and E. Potosina.",
+    },
+  ],
+};
+
+export default data;
